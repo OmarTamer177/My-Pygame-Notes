22a1476b5ed581efa3e748ad1e9245f05215ff94
OmarTamer177 <110217240+OmarTamer177@users.noreply.github.com>
2024-02-18 16:26:23 +0200

    Add files via upload

---
+++ 11-Displaying score.py
@@ -0,0 +1,100 @@
+#################################################################
+# Displaying Score
+# 1-Update score on every frame
+# 2-Put it on a surface
+# 3-Display that surface
+#################################################################
+import pygame, sys
+
+pygame.init()
+
+pygame.display.set_caption('Game Title')
+screen = pygame.display.set_mode((800, 400)) 
+clock = pygame.time.Clock()
+
+game_active = True
+
+sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha()
+ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
+
+font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+#Initializing score variable and update it as time passes 
+score = 0
+#and create a variable holding when the current game has started
+start_time = 0
+
+score_surface = font.render("score: " + str(score), False, (64, 64, 64))
+score_rect = score_surface.get_rect(center = (400,20))
+
+snail = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail.get_rect(bottomright = (800,300))
+
+player = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player.get_rect(midbottom = (80,300))
+player_gravity = 0
+player_jumping = False
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        #if game is over wait for player input to reset the game
+        if game_active:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not player_jumping:
+                    player_gravity = -18
+                    player_jumping = True
+        else:
+            if event.type == pygame.KEYDOWN:
+                game_active = True
+
+    if game_active:
+        screen.fill('black')
+        
+        screen.blit(sky_surface, (0,0))
+        screen.blit(ground_surface, (0,300))
+        
+        pygame.draw.rect(screen, '#c0e8ec' , score_rect)
+        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+
+        #Use pygame.time.get_ticks() method to get the current time in milliseconds, 
+        #and subtract the start time to calculate the actual current game start time
+        curr_time = pygame.time.get_ticks() - start_time
+        #then update score as time passes, considering making score NOT in millisec 
+        #and making it more readable
+        score = int(curr_time / 1000)
+
+        score_surface = font.render("score: " + str(score), False, (64, 64, 64))
+        screen.blit(score_surface, score_rect)
+
+        screen.blit(snail, snail_rect)
+        if snail_rect.right>0:
+            snail_rect.left -= 5
+        else:
+            snail_rect.left = 800
+
+        player_rect.y += player_gravity
+        if player_rect.bottom > 300: 
+            player_gravity = 0
+            player_rect.bottom = 300
+            player_jumping = False
+        else:
+            player_gravity += 1
+
+        if player_rect.colliderect(snail_rect):
+            game_active = False
+        
+        if not game_active:
+            print("game over")
+
+        screen.blit(player, player_rect)
+    else:
+        snail_rect.right = 800
+        player_rect.y = 300
+        #reset start time upon gameover
+        start_time = pygame.time.get_ticks()
+
+    pygame.display.update()
+    clock.tick(60)
